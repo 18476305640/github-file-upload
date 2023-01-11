d104e84896b9d635dfaff87c43694efd69b9499f
unknown <zhuangjie>
2023-01-11 20:14:57 +0800

    解决了获取仓库信息的一些问题

--- js/modules/index.js
+++ js/modules/index.js
@@ -4,9 +4,9 @@ import '../jquery.js'
 import '../utils.js'
 import '../compression.js'
 
-window.PlaceToSelect = function(current_repos = window.tmp_userAndRepo) {
-  if(!current_repos) current_repos = JSON.parse(localStorage.getItem("userAndRepo"));
-  if(!current_repos || !(current_repos instanceof Array)) return;
+window.PlaceToSelect = function (current_repos = window.tmp_userAndRepo) {
+  if (!current_repos) current_repos = JSON.parse(localStorage.getItem("userAndRepo"));
+  if (!current_repos || !(current_repos instanceof Array)) return;
   let options = "";
   for (let z = 0; z < current_repos.length; z++) {
     options += `<option value ='${current_repos[z].full_name}' >${current_repos[z].full_name}</option>`
@@ -20,10 +20,10 @@ $(function () {
     $("input[name='token']").val(configObj.token)
     // 下拉框的数据回显
     let option_nodes = $("select > option")
-     //回显userAndRepo时html前准备
+    //回显userAndRepo时html前准备
     let _userAndRepo = localStorage.getItem("userAndRepo")
     let _config = localStorage.getItem("config")
-    if( _userAndRepo || (_config == null) ) {
+    if (_userAndRepo || (_config == null)) {
       $("#userAndRepo_td").html(`
       <select name="userAndRepo">
         <option value ="">--需要检验token--</option>
@@ -31,13 +31,13 @@ $(function () {
       `);
       PlaceToSelect();
       // 恢复选中
-      for(let i = 0; i<option_nodes.length; i++) {
+      for (let i = 0; i < option_nodes.length; i++) {
         let option_node = $(option_nodes[i])
-        if(option_node.val() == configObj.userAndRepo) {
-          option_node.attr("selected",true);
+        if (option_node.val() == configObj.userAndRepo) {
+          option_node.attr("selected", true);
         }
       }
-    }else {
+    } else {
       $("#userAndRepo_td").html(`<input type="text" name="userAndRepo" placeholder="请手动输入，比如：“zhuangjie/img-repo”">`)
       $("input[name^='userAndRepo']").val(configObj.userAndRepo)
     }
@@ -46,11 +46,11 @@ $(function () {
     $("input[name='path']").val(configObj.path)
     $("input[name='dns']").val(configObj.dns)
     // 压缩配置回显
-    if(configObj.compression != null && configObj.compression == 1 ) {
-      $("input[name='compression']").attr('checked',true)
+    if (configObj.compression != null && configObj.compression == 1) {
+      $("input[name='compression']").attr('checked', true)
     }
     $("input[name='compression_config']").val(configObj.compression_config)
-    
+
   })()
   // 配置的保存
   let configButName = $("#config_control").html()
@@ -59,18 +59,18 @@ $(function () {
   }
   $("#config_control").click(function () {
     if ($("#config").is(":visible")) {
-      if(window.tmp_userAndRepo) {
-        localStorage.setItem("userAndRepo",JSON.stringify(window.tmp_userAndRepo))
+      if (window.tmp_userAndRepo) {
+        localStorage.setItem("userAndRepo", JSON.stringify(window.tmp_userAndRepo))
       }
       $("#config_control").html(configButName)
       // 执行保存配置的操作
       configObj.token = $("input[name='token']").val()
-      configObj.userAndRepo = $("select[name='userAndRepo']").val() ||  $("input[name='userAndRepo']").val()
+      configObj.userAndRepo = $("select[name='userAndRepo']").val() || $("input[name='userAndRepo']").val()
       configObj.branch = $("input[name='branch']").val()
       configObj.path = $("input[name='path']").val()
       configObj.dns = $("input[name='dns']").val()
-      configObj.compression = $("input[name='compression']").is(':checked')?1:0;
-      configObj.compression_config =  $("input[name='compression_config']").val();
+      configObj.compression = $("input[name='compression']").is(':checked') ? 1 : 0;
+      configObj.compression_config = $("input[name='compression_config']").val();
       localStorage.setItem("config", JSON.stringify(configObj));
       $("#tis").html(tis_content);
     } else {
@@ -90,7 +90,7 @@ $(function () {
   })
 
 
-  
+
 
   // 当token输入框失去焦点事件
   $("input[name^='token']").blur(function (e) {
@@ -105,64 +105,80 @@ $(function () {
     // 是否检验
     window.isCheck = false;
     // 如果超时，将显示输入框形式而不是下拉框
-    setTimeout(function() {
-      console.log("check",window.isCheck)
-      if(window.isCheck) return;
+    setTimeout(function () {
+      console.log("check", window.isCheck)
+      if (window.isCheck) return;
       $("#userAndRepo_td").html(`<input type="text" name="userAndRepo" placeholder="检验超时，请手动输入，比如：“zhuangjie/img-repo”">`)
-    },3000)
-    // 发起请求
-    $.ajax({
-      type: 'get',
-      url: "https://api.github.com/user/repos",
-      headers: {
-        'Authorization': 'token ' + token,
-        'Accept': 'application/vnd.github+json'
-      },
-      success(data) {
-        console.log("成功")
-        let current_repos = []
-        for (let i = 0; i < data.length; i++) {
-          let item = data[i]
-          // 只操作用户创建的
-          if (!item.has_issues) continue;
-          current_repos.push({
-            full_name: item.full_name,
-            pushed_at: new Date(item.pushed_at).getTime(),
-            default_branch: item.default_branch,
-            // avatar_url :item.owner.avatar_url
-          })
+    }, 3000)
+    
+    // 当输入token时，触发获取仓库信息显示，给用户选择
+    new Promise((resolve, reject) => {
+      // 根据token获取用户名，也可以获取仓库，但有的好像没有
+      $.ajax({
+        type: 'get',
+        url: "https://api.github.com/user/repos", //https://api.github.com/users/18476305640/repos
+        headers: {
+          'Authorization': 'token ' + token,
+          'Accept': 'application/vnd.github+json'
+        },
+        success(data) {
+          resolve(data[0].owner.login);
         }
-        // 排序
-        for (let j = 0; j < current_repos.length - 1; j++) {
-          for (let p = j + 1; p < current_repos.length; p++) {
-            if (current_repos[j].pushed_at < current_repos[p].pushed_at) {
-              let tmp = current_repos[j]
-              current_repos[j] = current_repos[p]
-              current_repos[p] = tmp;
-            }
+      });
+    }).then(username => {
+      // 根据用户名获取公开的仓库
+      return new Promise((resolve, reject) => {
+        $.ajax({
+          type: 'get',
+          url: `https://api.github.com/users/${username}/repos`,
+          success(data) {
+            // 公开的仓库
+            resolve(data);
+          }
+        });
+      })
+    }).then(allRepos => {
+      // 全部的仓库
+      let current_repos = [];
+      for (let i = 0; i < allRepos.length; i++) {
+        let repo = allRepos[i];
+        // 只操作用户创建的
+        if (!repo.has_issues) continue;
+        current_repos.push({
+          full_name: repo.full_name,
+          pushed_at: new Date(repo.pushed_at).getTime(),
+          default_branch: repo.default_branch,
+          // avatar_url :repo.owner.avatar_url
+        })
+      }
+      // 排序
+      for (let j = 0; j < current_repos.length - 1; j++) {
+        for (let p = j + 1; p < current_repos.length; p++) {
+          if (current_repos[j].pushed_at < current_repos[p].pushed_at) {
+            let tmp = current_repos[j]
+            current_repos[j] = current_repos[p]
+            current_repos[p] = tmp;
           }
         }
-        window.tmp_userAndRepo = current_repos
-        // 将内容放在下拉框中
-        // console.log("排序好的",current_repos)
-        PlaceToSelect(current_repos)
-        window.isCheck = true
-
-      },
-      error(err) {
-        console.log("失败")
-        console.log(err)
-        if (err.status == 401) {
-          alert("校验失败，请检查一下token是否填写错误~")
-        }
       }
-
+      window.tmp_userAndRepo = current_repos
+      // 将内容放在下拉框中
+      // console.log("排序好的",current_repos)
+      PlaceToSelect(current_repos)
+      window.isCheck = true
+    }).catch(error => {
+      console.log("失败信息：", error)
+      console.log(error)
+      if (error.status == 401) {
+        alert("校验失败，请检查一下token是否填写错误~")
+      }
     })
+
 
     $("select[name^='userAndRepo']").change(function () {
       let userAndRepo = $("select[name^='userAndRepo']").val();
       let current_repos = window.tmp_userAndRepo
-      if(userAndRepo == '' || !(current_repos instanceof Array )) return;
+      if (userAndRepo == '' || !(current_repos instanceof Array)) return;
       for (let i = 0; i < current_repos.length; i++) {
         let item = current_repos[i]
         if (item.full_name == userAndRepo) {
@@ -176,7 +192,7 @@ $(function () {
   })
   // 压缩的提示
   let tis = "";
-  $(".compressionTis").attr("title",tis = "宽高压缩比例 : 宽度低于多少时不压缩宽高 : 质量").click(function(e) {
+  $(".compressionTis").attr("title", tis = "宽高压缩比例 : 宽度低于多少时不压缩宽高 : 质量").click(function (e) {
     alert(tis);
     e.preventDefault();
   });
