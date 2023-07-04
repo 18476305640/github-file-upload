f85539ed56d7f0a7ae722c2d738f6fde3bb62006
zhuangjie <2119299531@qq.com>
2023-07-04 16:21:09 +0800

    解决存在的文件上传后没有复制的问题

--- js/modules/upload.js
+++ js/modules/upload.js
@@ -72,20 +72,10 @@ let githubUpload = function (fileName, fileData, isImage = true) {
       let initUrl = data.content.download_url
       let dnsUrl = dns(initUrl)
       // 将内容写到剪切板
-      let finallyUrl = dnsUrl;
-
-      if (! isImage) {
-        $("#resource_box").html(`<p style="color:#2cb144;" class="resource_box" > <a href="${initUrl}" class="copyUrl" >文件原链<i class="fa fa-clone" aria-hidden="true"></i></a>&nbsp;&nbsp;&nbsp;<a href="${dnsUrl}" class="copyUrl">加速链接<i class="fa fa-clone" aria-hidden="true"></i></a></p>`)
-        $("#msg").html($("#msg").html() + `<span style="background:#9e9831;" >( 默认复制加速链接，如果是一些特殊文件加速链接可能打不开，所以在这里给出了原链~ )<span>`)
-        // 给资源链接绑定copy功能
-        bindCopy(".resource_box", ".copyUrl", "href", "click");
-        transitionChangeTitle("success", 1000);
-        return;
-      }
       // 如果不是文件，那就是图片用md图片格式进行包装
       // console.log(">>> 正在上传的是图片");
-      finallyUrl = urlFormat(dnsUrl, "md")
-      navigator.clipboard.writeText(finallyUrl).then(function () {
+      let imgUrl = urlFormat(dnsUrl, "md")
+      navigator.clipboard.writeText(isImage?imgUrl:dnsUrl).then(function () {
         console.log('OK，Template copied to clipboard！')
         $("#msg").html($("#msg").html() + "<p id='uploadSuccessTis'>② 上传成功了，请查看剪切板！ヾ(^▽^*)))</p>")
         transitionChangeTitle("success", 1000);
@@ -95,11 +85,21 @@ let githubUpload = function (fileName, fileData, isImage = true) {
           initUrl,
           cdnURL: dns(initUrl)
         })
-        // 图片回显
-        $("#resource_box").html(`<img src="${dnsUrl}" />`)
+        if (! isImage) {
+          // 如果不是图片，显示下载链接复制选项
+          $("#resource_box").html(`<p style="color:#2cb144;" class="resource_box" > <a href="${initUrl}" class="copyUrl" >文件原链<i class="fa fa-clone" aria-hidden="true"></i></a>&nbsp;&nbsp;&nbsp;<a href="${dnsUrl}" class="copyUrl">加速链接<i class="fa fa-clone" aria-hidden="true"></i></a></p>`)
+          $("#msg").html($("#msg").html() + `<span style="background:#9e9831;" >( 默认复制加速链接，如果是一些特殊文件加速链接可能打不开，所以在这里给出了原链~ )<span>`)
+          // 给资源链接绑定copy功能
+          bindCopy(".resource_box", ".copyUrl", "href", "click");
+          transitionChangeTitle("success", 1000);
+        }else {
+          // 图片回显
+          $("#resource_box").html(`<img src="${dnsUrl}" />`)
+        }
+        
       }, function () {
         $("#msg").html("<span style='color:red'>Writing to the clipboard failed because you are not on this page :-( </span>")
-        $("#resource_box").html(`<p style="color:#2cb144;" class="resource_box" > <a href="${finallyUrl}" class="copyUrl" >手动复制（MD图片）<i class="fa fa-clone" aria-hidden="true"></i></a></p>`)
+        $("#resource_box").html(`<p style="color:#2cb144;" class="resource_box" > <a href="${dnsUrl}" class="copyUrl" >手动复制（MD图片）<i class="fa fa-clone" aria-hidden="true"></i></a></p>`)
         // 给资源链接绑定copy功能
         bindCopy(".resource_box", ".copyUrl", "href", "click");
         transitionChangeTitle("No way!", 500);
